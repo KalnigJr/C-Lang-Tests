df41099ae01c559941348ead556134c73edd1a84
KalnigJr <omarkallal4@gmail.com>
2023-05-29 01:30:54 +0000

    First C-Lang commit

---
+++ helloworld.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+
+
+     printf("hello\tworld\n happy to be here with you\n wish all the best for you all!");
+
+     //This is a comment
+     /*This is a block of comments 
+       and it does need to stop in one line*/
+    printf("today i learned print function loool\n");
+
+} 
+	
+
